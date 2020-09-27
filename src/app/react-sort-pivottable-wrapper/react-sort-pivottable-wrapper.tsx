201dcfcb53de385c0c253a4858f85aae7a4c0d73
andy23512 <andy23512@gmail.com>
2020-09-27 23:20:29 +0800

    Add plot support

--- src/app/react-sort-pivottable-wrapper/react-sort-pivottable-wrapper.tsx
+++ src/app/react-sort-pivottable-wrapper/react-sort-pivottable-wrapper.tsx
@@ -1,9 +1,22 @@
 import { useState } from 'react';
 import * as React from 'react';
 import PivotTableUI from 'react-pivottable/PivotTableUI';
+import TableRenderers from 'react-pivottable/TableRenderers';
+import Plot from 'react-plotly.js';
+import createPlotlyRenderers from 'react-pivottable/PlotlyRenderers';
+
+// create Plotly renderers via dependency injection
+const PlotlyRenderers = createPlotlyRenderers(Plot);
 
 // tslint:disable-next-line: typedef
 export function ReactSortPivottableWrapper(props) {
   const [state, setState] = useState({});
-  return <PivotTableUI data={props.data} onChange={setState} {...state} />;
+  return (
+    <PivotTableUI
+      data={props.data}
+      onChange={setState}
+      renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
+      {...state}
+    />
+  );
 }

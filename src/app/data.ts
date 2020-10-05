178c4d1f929455a9b24ef7c32f53fc614a347baa
andy23512 <andy23512@gmail.com>
2020-10-05 20:02:59 +0800

    Use time variable in data

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,50 +1,53 @@
+const time1 = '2020-09-24T00:00:00+08:00';
+const time2 = '2020-09-25T00:00:00+08:00';
+const time3 = '2020-09-26T00:00:00+08:00';
 export const data = [
   {
     user: 'User A',
     slide: 'Slide A',
-    time: '2020-08-20',
+    time: time1,
     value: 1,
   },
   {
     user: 'User A',
     slide: 'Slide A',
-    time: '2020-08-21',
+    time: time3,
     value: 2,
   },
   {
     user: 'User A',
     slide: 'Slide B',
-    time: '2020-08-20',
+    time: time1,
     value: 3,
   },
   {
     user: 'User A',
     slide: 'Slide B',
-    time: '2020-08-21',
+    time: time3,
     value: 4,
   },
   {
     user: 'User B',
     slide: 'Slide A',
-    time: '2020-08-20',
+    time: time1,
     value: 5,
   },
   {
     user: 'User B',
     slide: 'Slide A',
-    time: '2020-08-21',
+    time: time2,
     value: 6,
   },
   {
     user: 'User B',
     slide: 'Slide B',
-    time: '2020-08-20',
+    time: time1,
     value: 7,
   },
   {
     user: 'User B',
     slide: 'Slide B',
-    time: '2020-08-21',
+    time: time2,
     value: 8,
   },
 ];
